94e891a13edb5c5b22975dc9d54a0cbc965cb9f4
Tyrone Meijn <tyrone_meijn@hotmail.com>
2017-02-20 13:07:51 +0100

    Create register form
    Creates the register form and the corresponding reactive form elements in the component.

--- angular-src/src/app/app.module.ts
+++ angular-src/src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -34,7 +34,8 @@ const appRoutes: Routes = [
     BrowserModule,
     FormsModule,
     HttpModule,
-  RouterModule.forRoot(appRoutes)
+    ReactiveFormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- angular-src/src/app/components/register/register.component.ts
+++ angular-src/src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
+  form: FormGroup;
+  
 
-  constructor() { }
+  constructor(public _formBuilder: FormBuilder) { 
+    // Build the form group
+    this.form = this._formBuilder.group({
+      name: ['', [
+        Validators.required,
+      ]],
+      username: ['', [
+        Validators.required,
+      ]],
+      email: ['', [
+        Validators.required,
+      ]],
+      password: ['', [
+        Validators.required,
+      ]]
+    })
+  }
 
   ngOnInit() {
+  }
+
+  onRegisterSubmit() {
+    console.log('test');
   }
 
 }
